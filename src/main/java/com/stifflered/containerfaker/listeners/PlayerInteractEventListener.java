1863961fa4d2a85de169f3af35248f6febce98cd
Tim Stiffler-Dean <tim@stifflered.com>
2022-08-28 18:04:01 -0400

    Remove sound effects

--- src/main/java/com/stifflered/containerfaker/listeners/PlayerInteractEventListener.java
+++ src/main/java/com/stifflered/containerfaker/listeners/PlayerInteractEventListener.java
@@ -36,7 +36,6 @@ public class PlayerInteractEventListener implements Listener {
             event.setUseInteractedBlock(Event.Result.DENY);
 
             type.onOpen(event.getPlayer(), location);
-            event.getPlayer().playSound(block.getLocation(), Sound.BLOCK_SHULKER_BOX_OPEN, 0.5f, 1f);
         } else if (DISALLOW_BLOCKS.isTagged(block.getType())) {
             event.setUseInteractedBlock(Event.Result.DENY);
         }
